a633ea93d157e331956abf20a7df589611764298
Jakko Sikkar <jakko.sikkar@gmail.com>
2016-04-18 10:46:26 +0300

    save form windowstate in user settings

---
+++ Guinunit/UserSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using Eto.Forms;
+
+namespace Guinunit
+{
+    public sealed class UserSettings : ApplicationSettingsBase
+    {
+        public static UserSettings Default { get; } = new UserSettings();
+
+        private const string MAIN_WINDOW_STATE = "MainWindowState";
+
+        [UserScopedSetting]
+        [DefaultSettingValue("Normal")]
+        public WindowState MainWindowState
+        {
+            get { return (WindowState) this[MAIN_WINDOW_STATE]; }
+            set { this[MAIN_WINDOW_STATE] = value; }
+        }
+    }
+}
